9d6db35622060a848078aaba707d23fe5609a7ba
hsolbrig <solbrig.harold@mayo.edu>
2018-01-12 19:14:14 -0600

    Adds SchemaLoader

---
+++ pyshex/utils/schema_loader.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2018, Mayo Clinic
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+#     list of conditions and the following disclaimer.
+#
+#     Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#     Neither the name of the Mayo Clinic nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+from typing import cast, Union, TextIO
+from urllib.request import urlopen
+
+from ShExJSG import ShExJ
+from pyjsg.jsglib import jsg
+from pyshexc.parser_impl import generate_shexj
+
+
+class SchemaLoader:
+    def __init__(self, base_location=None, redirect_location=None) -> None:
+        self.base_location = base_location
+        self.redirect_location = redirect_location
+
+    def load(self, schema_location: Union[str, TextIO]) -> ShExJ.Schema:
+        """ Load a ShEx Schema from schema_location
+
+        :param schema_location:  name or file-like object to deserialize
+        :return: ShEx Schema represented by schema_location
+        """
+        if isinstance(schema_location, str):
+            real_schema_location = self._location_rewrite(schema_location)
+            if ':' in real_schema_location:
+                schema_txt = urlopen(real_schema_location).read().decode()
+            else:
+                with open(real_schema_location) as schema_file:
+                    schema_txt = schema_file.read()
+        else:
+            schema_txt = schema_location.read()
+        return self.loads(schema_txt)
+
+    @staticmethod
+    def loads(schema_txt: str) -> ShExJ.Schema:
+        """ Parse and return schema as a ShExJ Schema
+
+        :param schema_txt: ShExC or ShExJ representation of a ShEx Schema
+        :return: ShEx Schema representation of schema
+        """
+        if schema_txt.strip()[0] == '{':
+            return cast(ShExJ.Schema, jsg.loads(schema_txt, ShExJ))
+        else:
+            return generate_shexj.parse(schema_txt)
+
+    def _location_rewrite(self, schema_location: str) -> str:
+        return schema_location.replace(self.base_location, self.redirect_location) \
+            if self.base_location and schema_location.startswith(self.base_location) else schema_location
---
+++ tests/test_pyshex_utils/test_schema_loader.py
@@ -0,0 +1,122 @@
+# Copyright (c) 2018, Mayo Clinic
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+#     list of conditions and the following disclaimer.
+#
+#     Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#     Neither the name of the Mayo Clinic nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+
+import os
+from rdflib import RDF
+
+from pyshex.utils.schema_loader import SchemaLoader
+
+
+class SchemaLoaderTestCase(unittest.TestCase):
+    def test_loads_shexc(self):
+        """ Load a schema string and test a couple of elements """
+        loader = SchemaLoader()
+        schema = loader.loads("""<http://a.example/S1> {
+   ( <http://a.example/p1> .|
+     <http://a.example/p2> .|
+     <http://a.example/p3> .|
+     <http://a.example/p4> .
+   ){2,3}
+}""")
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+        self.assertEqual({"http://a.example/p1",
+                          "http://a.example/p2",
+                          "http://a.example/p3",
+                          "http://a.example/p4"}, {e.predicate for e in schema.shapes[0].expression.expressions})
+
+    def test_loads_shexj(self):
+        """ Load a schema string and test a couple of elements """
+        loader = SchemaLoader()
+        schema = loader.loads("""{
+  "@context": "http://www.w3.org/ns/shex.jsonld",
+  "type": "Schema",
+  "shapes": [
+    {
+      "id": "http://a.example/S1",
+      "type": "Shape",
+      "expression": {
+        "type": "TripleConstraint",
+        "predicate": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+      }
+    }
+  ]
+}""")
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+        self.assertEqual(str(RDF.type), schema.shapes[0].expression.predicate)
+
+    def test_load_shexc(self):
+        loader = SchemaLoader()
+
+        # Local file name
+        fileloc = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'data', 'startCode3.shex')
+        schema = loader.load(fileloc)
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+
+        # Local file object
+        with open(fileloc) as f:
+            schema2 = loader.load(f)
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+
+        # URL
+        fileurl = "https://raw.githubusercontent.com/shexSpec/shexTest/2.0/schemas/startCode3.shex"
+        schema = loader.load(fileurl)
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+
+    def test_load_shexj(self):
+        loader = SchemaLoader()
+
+        # Local file name
+        fileloc = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'data', 'startCode3.json')
+        schema = loader.load(fileloc)
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+
+        # Local file object
+        with open(fileloc) as f:
+            schema2 = loader.load(f)
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+
+        # URL
+        fileurl = "https://raw.githubusercontent.com/shexSpec/shexTest/2.0/schemas/startCode3.json"
+        schema = loader.load(fileurl)
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+
+
+    def test_location_rewrite(self):
+        loader = SchemaLoader()
+        # Note: Deliberately a bad URL to make sure this works
+        loader.base_location = "https://raw.githubusercontent.com/shexSpec/shexTest/2.0/schemasz/"
+        loader.redirect_location = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'data') + '/'
+        fileloc = loader.base_location + 'startCode3.shex'
+        schema = loader.load(fileloc)
+        self.assertEqual("http://a.example/S1", schema.shapes[0].id)
+
+
+if __name__ == '__main__':
+    unittest.main()
