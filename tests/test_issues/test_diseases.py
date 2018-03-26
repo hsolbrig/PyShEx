00bf4d6cca27becd07c15f95fb587455de65f22b
hsolbrig <solbrig@earthlink.net>
2018-03-26 14:20:52 -0500

    Remove note about entry #9
    The issue is actually that wikidata is throttling us -- hangs will be random

--- tests/test_issues/test_diseases.py
+++ tests/test_issues/test_diseases.py
@@ -13,9 +13,8 @@ class WikiDiseasesTestCase(WikiDataTestCase):
     def test_diseases(self):
         test_data_base = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'wikidata', 'disease'))
 
-        # TODO: Entry #9 causes both this process AND shex.js to hang
         rslts = self.run_test("https://raw.githubusercontent.com/SuLab/Genewiki-ShEx/master/diseases/manifest_100.json",
-                              num_entries=8, debug=False, debug_slurps=False, save_graph_dir=test_data_base)
+                              num_entries=8, debug=False, debug_slurps=True, save_graph_dir=test_data_base)
         for rslt in rslts:
             print(f"{'CONFORMS' if rslt.result else 'FAIL'}: {rslt.focus}")
         self.assertTrue(all(r.result for r in rslts))
