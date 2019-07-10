ef129d3fd854f69cd015202be7677f417e486ab7
hsolbrig <solbrig@jhu.edu>
2019-07-10 18:24:37 -0500

    Fix issue #51 -- include nested te labels and report errors at runtime.
    This does not address the other part of issue #51 -- unresolved labels
    are not detected until referenced.  It should be noted, however, that,
    at the moment, unresolved shape references are not detected either.

---
+++ tests/test_issues/test_issue_51.py
@@ -0,0 +1,63 @@
+import unittest
+from pprint import pprint
+
+from rdflib import Graph, Namespace, RDF
+
+from pyshex import ShExEvaluator
+
+BASE = Namespace("https://w3id.org/biolink/vocab/")
+
+rdf = f"""
+@prefix : <{BASE}> .
+@prefix rdf: <{RDF}> .
+:s rdf:type :X .
+"""
+
+shex = f"""
+BASE <{BASE}>
+
+<BiologicalProcess> ( 
+    {{
+        ( $<BiologicalProcess_tes> a [ <BiologicalProcessOrActivity> ] ?;
+          a [ <BiologicalProcess> ]
+        )
+    }} OR @<X>
+)
+
+<X> {{&<BiologicalProcess_tes>; a [<X>]}}
+"""
+
+shex2 = f"""
+BASE <{BASE}>
+
+<BiologicalProcess> ( 
+    {{
+        ( $<BiologicalProcess_tes> a [ <BiologicalProcessOrActivity> ] ?;
+          a [ <BiologicalProcess> ]
+        )
+    }} OR @<X>
+)
+
+<X> {{&<missing>}}
+"""
+
+BASE = Namespace("https://w3id.org/biolink/vocab/")
+
+
+class Issue51TestCase(unittest.TestCase):
+    def test_inner_te(self):
+        """ Test recognition of an inner triple expression """
+
+        e = ShExEvaluator(rdf=rdf, schema=shex, focus=BASE.s, start=BASE.X).evaluate()
+        self.assertTrue(e[0].result)
+
+    def test_te_message(self):
+        """ Test the error message (and eventually the startup test) """
+        e = ShExEvaluator(rdf=rdf, schema=shex2, focus=BASE.s, start=BASE.X).evaluate()
+        self.assertFalse(e[0].result)
+        self.assertEqual('  Testing :s against shape https://w3id.org/biolink/vocab/X\n'
+                         '    https://w3id.org/biolink/vocab/missing: Reference not found', e[0].reason)
+
+
+if __name__ == '__main__':
+    unittest.main()
