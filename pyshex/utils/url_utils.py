77126bd532cbcbc823fe5469fe7e96bf2f8f510b
Ben McAlister <benmcalister@users.noreply.github.com>
2022-01-14 19:09:53 +0000

    Replace '/' with '\'

--- pyshex/utils/url_utils.py
+++ pyshex/utils/url_utils.py
@@ -9,10 +9,10 @@ def generate_base(path: str) -> str:
     :param path: file location or url
     :return: file location or url sans actual name
     """
-    if ':' in path:
+    if '://' in path:
         parts = urlparse(path)
         parts_dict = parts._asdict()
         parts_dict['path'] = os.path.split(parts.path)[0] if '/' in parts.path else ''
         return urlunparse(ParseResult(**parts_dict)) + '/'
     else:
-        return (os.path.split(path)[0] if '/' in path else '') + '/'
+        return (os.path.split(path)[0] if '/' in path else '').replace('\\', '/') + '/'
